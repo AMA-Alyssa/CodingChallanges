9528bb73e03d413106e145d09eb9a100655ab2c0
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-07-29 05:51:32 -0500

    descending order PREP layout

---
+++ codewars7-29-22.js
@@ -0,0 +1,19 @@
+//PREP
+//Parameters
+//Return
+//Example    
+//Pseudo 
+
+//P: You are given a number and it will return a new number that is rearrange to become the highest possible number
+//R: A new number thats the highest number combo
+//E: 1234 -> 4321
+//P:
+
+function descendingOrder(n){
+    //convert into a string
+    //split the string
+    //rearrange the elements in the array to highest to smallest
+    //join string
+    //make a number 
+    //sort (a,b) => b-a
+  }
