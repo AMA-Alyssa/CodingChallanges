3ebbe9c67e647d7846d2d858223307e23178bb1b
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-08-02 19:34:42 -0500

    completed cc

--- codewars8-1-22.js
+++ codewars8-1-22.js
@@ -14,6 +14,32 @@
 //Results the count of divisors   
 //Examples: 5 ==>2 (1,5)
 //Psuedo 
+//Psuedo 
+
+//assign variable to 0
+//for loop
+//if statement in for loop
+// if n remainder of i = 0, then 
+
+
 function getDivisorsCnt(n){
-    // loop 
+    let num = 0;
+    //this is setting our count to 0
+    for( let i =1; i <=n; i++){
+        // let i=1; i <=5, i++
+        //this is looping through each number until we hit that given number (n)
+        if (n %i == 0){
+            // 5 % 1 == 0
+            // 5 % 2 == 1
+            //5 % 3 ==2
+            //5 % 4 == 1
+            //5 % 5 == 0
+            num +=1;
+            //this is adding 1 to the count
+        }
+
+    }
+    return num;
+    //this is returning our total count of divisors
+}
     
