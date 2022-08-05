e5d3b732d9c6e03e01b369c89e4f5184114e0f51
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-08-04 20:32:04 -0500

    cc

---
+++ codewars8-4-22.js
@@ -0,0 +1,26 @@
+// Complete the solution so that it returns true if the first argument(string) 
+// passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//PREP
+
+//Parameters
+//Results
+//Example
+//Psuedo
+
+//P - make a function that returns true if the given first string  ends with the 2nd string
+//returns true or false
+//E - 'abcde', 'cde' --> true, abe, d  --> false
+//P
+
+function solution(str, ending){
+    // function solution('abc', 'bc')
+   //make str and ending  --seperate.
+   //cd 
+
+  }
