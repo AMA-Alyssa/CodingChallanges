2857dba3f6b20e65c9263d66884aca200ba9a586
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-08-11 20:54:15 -0500

    cc

---
+++ codewars8-11-22.js
@@ -0,0 +1,21 @@
+ //You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+//Write a program that returns the girl's age (0-9) as an integer.
+
+//Assume the test input string is always a valid string. 
+//For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+//PREP
+//parameters -> we are given a string and it will always have the number first in the string. It will always be positive and single digit
+//Results -> the reult is a number
+//Example -> the string says "6 years today" and I return 6
+//Psuedo ->
+
+
+
+function getAge(inputString){
+    //seperate string and put in an array
+    let seperateString = inputString.split(' ');
+    //take the index of 0 of the that array
+    //convert into an number
+    //return number
+    }
