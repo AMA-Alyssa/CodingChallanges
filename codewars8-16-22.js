4c0ddb1b5647316f0c15539a36196a501be61292
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-08-16 16:59:39 -0500

    started cc

---
+++ codewars8-16-22.js
@@ -0,0 +1,21 @@
+// Check to see if a string has the same amount of 'x's and 'o's. 
+// The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+//PREP
+//P if string has = x & o, then true or if no x & o's then true. not = then false.
+//   it is case sensentive
+//r return true or false
+//example  - look above
+//peusdo 
+
+function XO(str) {
+    //code here
+}
