16c4e06ccd0c8627b4859cd5b96e4fe99ed4c07f
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-08-25 15:35:44 -0500

    another cc

--- codewars8-25-22.js
+++ codewars8-25-22.js
@@ -2,3 +2,11 @@ function solution(str){
     let newStr = str.split('').reverse().join('')
     return newStr;
   }
+
+  function removeChar(str){
+ 
+    let newArray = str.split('');
+    let nAR = newArray.shift();
+    let oMG = newArray.pop();
+    return newArray.join('')
+  };
