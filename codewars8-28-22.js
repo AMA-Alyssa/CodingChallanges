2d70fae54a3490d251ebc8efc04cbea5b37baae0
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-08-28 19:39:25 -0500

    boolean

---
+++ codewars8-28-22.js
@@ -0,0 +1,10 @@
+function booleanToString(b){
+    //P given a boolean value 
+    //Return a string
+    //E - true --> 'true'
+    //P  use toSting() method
+     
+    let newStr = b.toString();
+    return newStr;
+    
+  }
