8c10def394d4b6e19e799e10d2021883e64b8227
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-08-28 19:50:14 -0500

    includes()

---
+++ codewars8-27-22.js
@@ -0,0 +1,9 @@
+function check(a, x) {
+    // P given an array and a value it can be a string and or number
+    //R return true or false if the value is in the given array
+    //E  given [a,b,c,d,c,e,1,2,3] value is c --> return true
+    //use includes()
+    
+    return a.includes(x);
+    
+  }
