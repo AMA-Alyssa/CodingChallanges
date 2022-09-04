b710524cb43481419e1e533dde394f235b93fac9
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-09-04 12:16:15 -0500

    alphabet schema

--- models/Todo.js
+++ models/Todo.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose')
-
-const TodoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true
-  }
-})
-
-module.exports = mongoose.model('Todo', TodoSchema)
+const alphabetSchema = new mongoose.Schema({
+    letter: {
+      type: String,
+      required: true,
+    },
+    exercise: {
+        type: String,
+        required: true,
+      },
+    completed: {
+      type: Boolean,
+      required: true,
+    }
+  })
+  
+  module.exports = mongoose.model('alphabet', alphabetSchema)
