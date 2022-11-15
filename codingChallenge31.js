9a669af695368531c0f7ca2ba9259697bbd44e5c
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2022-11-15 17:29:39 -0600

    oddNums

---
+++ codingChallenge31.js
@@ -0,0 +1,36 @@
+
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+// sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
+
+function sortArray(arr){
+    //filter -> sort
+    //map
+    oddNums = arr.filter(n => n % 2 !== 0).sort((a,b) => a-b)
+    //console.log(oddNums)
+  
+    //map
+    return arr.map(n => n% 2 === 0 ? n : oddNums.shift())
+  }
+  
+  function sortArray(arr){
+    //filter -> sort
+    //map
+    oddNums = arr.filter(n => n % 2 !== 0).sort((a,b) => b-a)
+    //console.log(oddNums)
+  
+    //map
+    return arr.map(n => n% 2 === 0 ? n : oddNums.pop()) //pop saves time because shift is happening linera. Pop is happening constant
+  }
+  
+  //the shift is more complex.
+  
+  console.log(sortArray([7,2, 1]))  //=>  [1,2, 7]
+  
+  console.log(sortArray([5, 8, 6, 3, 4])) //=>  [3, 8, 6, 5, 4]
